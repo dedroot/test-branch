f62a7a0918500f53e2f84507c5578b977cc12cac
Thomas Pellisier <thomas.pellissier.pro@proton.me>
2023-09-20 13:32:30 +0200

    chore(hello.py): Add asking age (just for testing tag)

--- hello.py
+++ hello.py
@@ -1,1 +1,16 @@
 print("create commit for testing tags and auto changelog")
+
+try:
+    age = int(input("How old are you? : "))
+except ValueError:
+    print("Enter a valid integer!")
+    exit()
+
+if age > 130:
+    print("Are you lying?")
+    exit()
+if age <= 2:
+    print("How do you have the permission to use computer?")
+    exit()
+
+print(age)
